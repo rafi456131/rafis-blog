71b2f6d1764c83394cd10a702ff714442b8667b5
RAFIUL ASIF <u1313264@umail.utah.edu>
2024-03-06 20:53:33 -0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Projects from './pages/Projects';
+
+
 function App() {
   return (
-    <h1 className="text-6xl text-red-500">
-      This is Rafi
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/projects" element={<Projects />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
