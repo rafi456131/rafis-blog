8ce66cf0d867e341a602172a7e7dabf6956c98d5
RAFIUL ASIF <u1313264@umail.utah.edu>
2024-03-07 17:27:11 -0700

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Dashboard from "./pages/Dashboard"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Projects from './pages/Projects';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
-
+//https://www.youtube.com/watch?v=vqXLGX0szIQ
+//https://bestofjs.org/projects/react-router
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/dashboard" element={<Dashboard />}/>
-      <Route path="/sign-in" element={<SignIn />}/>
-      <Route path="/sign-up" element={<SignUp />}/>
-      <Route path="/projects" element={<Projects />}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
